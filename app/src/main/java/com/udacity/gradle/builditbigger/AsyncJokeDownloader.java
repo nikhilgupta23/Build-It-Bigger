96ed58934b847cb70df48d4435644558a9677163
nikhilgupta23 <nanonikhil@gmail.com>
2016-12-18 22:11:09 +0530

    Update AsyncJokeDownloader to prevent Internal Server Error (500).
    The error used to come up when the API call to the host machine was made from a client phone.

--- app/src/main/java/com/udacity/gradle/builditbigger/AsyncJokeDownloader.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/AsyncJokeDownloader.java
@@ -6,6 +6,8 @@ import android.os.AsyncTask;
 import com.example.nikhilgupta.backend.myApi.MyApi;
 import com.google.api.client.extensions.android.http.AndroidHttp;
 import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
+import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
 
 import java.io.IOException;
 
@@ -17,25 +19,31 @@ import java.io.IOException;
 public class AsyncJokeDownloader {
     private IDownloadListener downloadListener;
     private static MyApi myApiService = null;
-    public AsyncJokeDownloader( IDownloadListener downloadListener )
+    protected AsyncJokeDownloader( IDownloadListener downloadListener )
     {
         this.downloadListener = downloadListener;
     }
 
-    public void downloadJoke()
+    protected void downloadJoke()
     {
         new EndpointsAsyncTask().execute();
     }
-    class EndpointsAsyncTask extends AsyncTask<Void, Void, String> {
+    private class EndpointsAsyncTask extends AsyncTask<Void, Void, String> {
         private Context context;
 
         @Override
         protected String doInBackground(Void... params) {
             if(myApiService == null) {  // Only do this once
+                //Insert IP address of the host machine of the network below.
                 MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(), new AndroidJsonFactory(), null)
-                        .setRootUrl("http://192.168.0.2:8080/_ah/api/");
+                        .setRootUrl("http://192.168.0.2:8080/_ah/api/")
+                        .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
+                            @Override
+                            public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
+                                abstractGoogleClientRequest.setDisableGZipContent(true);
+                            }
+                        });
                 // end options for devappserver
-
                 myApiService = builder.build();
             }
 
